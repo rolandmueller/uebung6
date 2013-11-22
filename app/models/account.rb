c186fea3dea0d9d6781913c5a1840215800542ef
mTodisco <mtodisco@hwr-berlin.de>
2013-11-22 23:24:34 +0100

    Aufgabe 13-15

--- app/models/account.rb
+++ app/models/account.rb
@@ -5,37 +5,37 @@ class Account < ActiveRecord::Base
 	validates :number, presence: true, uniqueness: true
 	validates :balance, presence: true, numericality: {greater_than_or_equal_to: 0.0}
 
-	def withdraw(amount)
-                new_balance = self.balance - amount
-                if amount > 0 && new_balance >= 0
-                        self.update_attributes(:balance => new_balance)
-                        create_transaction(amount, 'Auszahlung', new_balance, self.id)
+	def withdraw(betrag)
+                kontostand_neu = self.balance - betrag
+                if betrag > 0 && kontostand_neu >= 0
+                        self.update_attributes(:balance => kontostand_neu)
+                        create_transaction(betrag, 'Auszahlung', kontostand_neu, self.id)
                 end
         end
 
-        def deposit(amount)
-                if amount > 0
-                        new_balance = self.balance + amount
-                        self.update_attributes(:balance => new_balance)
-                        create_transaction(amount, 'Einzahlung', new_balance, self.id)
+        def deposit(betrag)
+                if betrag > 0
+                        kontostand_neu = self.balance + betrag
+                        self.update_attributes(:balance => kontostand_neu)
+                        create_transaction(betrag, 'Einzahlung', kontostand_neu, self.id)
                 end
         end
 
-        def transfer(amount, target_account_number)
-                new_balance_for_self = self.balance - amount
+        def transfer(betrag, target_account_number)
+                kontostand_neu_for_self = self.balance - betrag
                 target_account = Account.find_by_number(target_account_number)
 
                 puts target_account.number
 
-                if amount > 0 && new_balance_for_self >= 0 && target_account != nil
+                if betrag > 0 && kontostand_neu_for_self >= 0 && target_account != nil
 
-                        self.update_attributes(:balance => new_balance_for_self)
+                        self.update_attributes(:balance => kontostand_neu_for_self)
                         
-                        new_balance_for_target_account = target_account.balance + amount
-                        target_account.update_attributes(:balance => new_balance_for_target_account)
+                        kontostand_neu_for_target_account = target_account.balance + betrag
+                        target_account.update_attributes(:balance => kontostand_neu_for_target_account)
 
-                        create_transaction(amount, "transfer zu #{target_account_number}", new_balance_for_self, self.id)
-                        create_transaction(amount, "transfer von #{self.number}", new_balance_for_target_account, target_account.id)
+                        create_transaction(betrag, "transfer zu #{target_account_number}", kontostand_neu_for_self, self.id)
+                        create_transaction(betrag, "transfer von #{self.number}", kontostand_neu_for_target_account, target_account.id)
 
                 end
         end
@@ -54,7 +54,7 @@ class Account < ActiveRecord::Base
 
         private
 
-        def create_transaction(amount, reference, new_balance, id)
-                Transaction.create(:amount => amount,:description => reference, :balance_after_transaction => new_balance, :account_id => id)
+        def create_transaction(betrag, referenz, kontostand_neu, id)
+                Transaction.create(:betrag => betrag,:description => referenz, :balance_after_transaction => kontostand_neu, :account_id => id)
         end
 end
